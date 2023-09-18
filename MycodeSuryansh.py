6bea2f06d9e433c4e47e1bbda70b53c9eb89a1de
suryansh55 <suryanshankur55@gmail.com>
2023-09-18 17:45:59 -0600

    added code new

--- MycodeSuryansh.py
+++ MycodeSuryansh.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python3
 
-from ev3dev.ev3 import *
+from ev3dev2.motor import LargeMotor
+from time import sleep
 
-# Connect the motors to the appropriate ports.
-left_motor = LargeMotor('outB')
-right_motor = LargeMotor('outC')
+# Connect the motor to the appropriate port.
+motor = LargeMotor('outB')
 
-# Set the motor speed and time to create the lemniscate shape.
-speed = 300  # You can adjust this value to control the speed of the robot.
-duration = 10  # You can adjust this value to control the duration of movement.
+# Set the initial speed and increment value.
+initial_speed = 0  # Starting speed (0% power)
+max_speed = 50     # Maximum speed (50% power)
+increment = 5      # Speed increment (5% power)
+delay = 1           # Delay between speed increments (1 second)
 
-# Function to move both motors forward.
-def move_forward():
-    left_motor.run_forever(speed_sp=speed)
-    right_motor.run_forever(speed_sp=speed)
-
-# Function to move both motors backward.
-def move_backward():
-    left_motor.run_forever(speed_sp=-speed)
-    right_motor.run_forever(speed_sp=-speed)
-
-# Function to stop both motors.
-def stop_motors():
-    left_motor.stop()
-    right_motor.stop()
-
-# Main code
 try:
-    while True:
-        move_forward()
-        time.sleep(duration)
-        stop_motors()
-        move_backward()
-        time.sleep(duration)
-        stop_motors()
+    while initial_speed <= max_speed:
+        motor.on(initial_speed)  # Set the motor speed
+        print(f"Motor Speed: {initial_speed}%")
+        sleep(delay)
+        initial_speed += increment
+
+    # Once the maximum speed is reached, hold it for a while.
+    motor.on(max_speed)
+    sleep(5)  # Hold for 5 seconds
+
+    # Gradually decrease the speed back to 0.
+    while initial_speed >= 0:
+        motor.on(initial_speed)
+        print(f"Motor Speed: {initial_speed}%")
+        sleep(delay)
+        initial_speed -= increment
+
+    # Turn off the motor.
+    motor.off()
 
 except KeyboardInterrupt:
-    stop_motors()
+    motor.off()
